ae98f2eb7365ca9d2980b9afd2cc795294ee53d8
Zidane Innis <zidaneinnis@gmail.com>
2023-01-07 15:21:50 -0500

    Adding Individual Meal Items & Displaying Them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import MealItem from "./components/Meals/MealItem/MealItem";
 import Meals from "./components/Meals/Meals";
 
 function App() {
