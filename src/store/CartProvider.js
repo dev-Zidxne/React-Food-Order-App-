a7870185aea4e784e98de837d3dd8b6572559481
Zidane Innis <zidaneinnis@gmail.com>
2023-01-08 00:07:12 -0500

    Working with Refs & Forward Refs

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -30,7 +30,7 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
 
-  const removeItemFromCartHandler = (item) => {
+  const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
